3ab114da4a29cf9140a4b5abe897f95c4cd060fc
myself <akwerosophia@gmail.com>
2026-01-11 23:00:23 +0300

    enter

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ def main():
     print("Welcome to my first GitHub project!")
     name = input(" What is your name? ")
     print(f"Nice to meet you, {name}!")
-
+    
 if __name__ == "__main__":
     main()
